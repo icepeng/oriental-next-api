13827a54da7b8610c09d2d0372aa983ab40ced68
ha <typ0@naver.com>
2018-08-02 13:14:06 +0900

    boomsdat

--- utils/card-generate.js
+++ utils/card-generate.js
@@ -1,35 +1,42 @@
 const list = require('./input.json');
 const changeCase = require('change-case');
 const fs = require('fs-extra');
+const prev = require('./prev.json');
 
-const ko = list.reduce((obj, item) => {
-  return {
-    ...obj,
-    [changeCase.paramCase(item.cardNameSlug)]: {
-      imgLink: item.localizedImage.ko_KR,
-      name: '',
-    },
-  };
-}, {});
+const ko = list
+  .sort((a, b) => a.class > b.class ? 1 : -1)
+  .reduce((obj, item) => {
+    return {
+      ...obj,
+      [changeCase.paramCase(item.cardNameSlug)]: {
+        imgLink: item.localizedImage.ko_KR,
+        name: '',
+      },
+    };
+  }, {});
 
-const en = list.reduce((obj, item) => {
-  return {
-    ...obj,
-    [changeCase.paramCase(item.cardNameSlug)]: {
-      imgLink: item.localizedImage.en_US,
-      name: '',
-    },
-  };
-}, {});
+const en = list
+  .sort((a, b) => a.class > b.class ? 1 : -1)
+  .reduce((obj, item) => {
+    return {
+      ...obj,
+      [changeCase.paramCase(item.cardNameSlug)]: {
+        imgLink: item.localizedImage.en_US,
+        name: '',
+      },
+    };
+  }, {});
 
-const cards = list.map(item => {
-  return {
-    id: changeCase.paramCase(item.cardNameSlug),
-    class: item.class,
-    rarity: '',
-    cost: '',
-  };
-});
+const cards = list
+  .filter(item => !prev[changeCase.paramCase(item.cardNameSlug)])
+  .map(item => {
+    return {
+      id: changeCase.paramCase(item.cardNameSlug),
+      class: item.class,
+      rarity: '',
+      cost: '',
+    };
+  });
 
 const druid = cards.filter(x => x.class === 'DRUID');
 const hunter = cards.filter(x => x.class === 'HUNTER');
@@ -43,16 +50,16 @@ const warlock = cards.filter(x => x.class === 'WARLOCK');
 const warrior = cards.filter(x => x.class === 'WARRIOR');
 
 Promise.all([
-  // fs.writeJSON('ko-kr.cards.json', ko, { spaces: 2 }),
-  // fs.writeJSON('en-us.cards.json', en, { spaces: 2 }),
-  // fs.writeJSON('cards_DRUID.json', druid, { spaces: 2 }),
-  // fs.writeJSON('cards_HUNTER.json', hunter, { spaces: 2 }),
-  // fs.writeJSON('cards_MAGE.json', mage, { spaces: 2 }),
-  // fs.writeJSON('cards_NEUTRAL.json', neutral, { spaces: 2 }),
+  fs.writeJSON('ko-kr.cards.json', ko, { spaces: 2 }),
+  fs.writeJSON('en-us.cards.json', en, { spaces: 2 }),
+  fs.writeJSON('cards_DRUID.json', druid, { spaces: 2 }),
+  fs.writeJSON('cards_HUNTER.json', hunter, { spaces: 2 }),
+  fs.writeJSON('cards_MAGE.json', mage, { spaces: 2 }),
+  fs.writeJSON('cards_NEUTRAL.json', neutral, { spaces: 2 }),
   fs.writeJSON('cards_PALADIN.json', paladin, { spaces: 2 }),
-  // fs.writeJSON('cards_PRIEST.json', priest, { spaces: 2 }),
-  // fs.writeJSON('cards_ROGUE.json', rogue, { spaces: 2 }),
-  // fs.writeJSON('cards_SHAMAN.json', shaman, { spaces: 2 }),
-  // fs.writeJSON('cards_WARLOCK.json', warlock, { spaces: 2 }),
-  // fs.writeJSON('cards_WARRIOR.json', warrior, { spaces: 2 }),
+  fs.writeJSON('cards_PRIEST.json', priest, { spaces: 2 }),
+  fs.writeJSON('cards_ROGUE.json', rogue, { spaces: 2 }),
+  fs.writeJSON('cards_SHAMAN.json', shaman, { spaces: 2 }),
+  fs.writeJSON('cards_WARLOCK.json', warlock, { spaces: 2 }),
+  fs.writeJSON('cards_WARRIOR.json', warrior, { spaces: 2 }),
 ]);
